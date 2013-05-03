292d1c1ede0abfb49c26c93c5933214daf8fd4be
Adhish Majumdar <adhishm@gmail.com>
2013-05-03 09:39:50 +0200

    Removed doc

--- doc/html/search/variables_73.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['shearstrains',['shearStrains',['../d1/d3c/classStrain.html#afbc4d1b573860bda614a43f767b8b272',1,'Strain']]],
-  ['shearstresses',['shearStresses',['../d1/d1c/classStress.html#a77e8705e56c2fb56826a638edf3f78bf',1,'Stress']]]
-];
