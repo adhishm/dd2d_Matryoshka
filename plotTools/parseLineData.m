f3dd07478634b7409bbbca579009f40cfa047dd7
Adhish Majumdar <adhishm@gmail.com>
2013-09-10 11:47:56 +0200

    Defined functions parseLineData and plotSlipPlane

--- plotTools/parseLineData.m
+++ plotTools/parseLineData.m
@@ -1,5 +1,36 @@
 function numbers = parseLineData (lineData, delimiter)
 %% numbers = parseLineData (lineData, delimiter)
-%  This function takes line of characters
+%  This function takes line of characters that contains numbers (as text)
+%  separated by a delimiter. The function separates out these numbers and
+%  returns them in the vector numbers.
+
+    %% Find number of characters
+    nChar = size(lineData,2);   % Number of columns
+    
+    %% Pre-allocate numbers for speed
+    numbers = zeros(1,nChar);   % There cannot be more numbers than there are characters
+    
+    %% Browse through characters till the end of the line is reached
+    charCount = 1;
+    dataCount = 0;
+    while (charCount <= nChar)
+        %% Find the start and end point of each data
+        startPosition = charCount;
+        while (lineData(charCount) ~= delimiter)
+            charCount = charCount + 1;
+        end
+        endPosition = charCount - 1;    % The position before the delimiter
+        
+        %% Separate the data, convert to number, insert into vector
+        data = lineData(startPosition:endPosition);
+        dataCount = dataCount + 1;
+        numbers(1,dataCount) = str2double(data);
+        clear data;
+        
+        charCount = charCount + 1;  % Increment charCount to move beyond the delimiter
+    end
+    
+    %% Remove padding zeros
+    numbers = numbers(1,1:dataCount);
 
 end
--- plotTools/plotSlipPlane.m
+++ plotTools/plotSlipPlane.m
@@ -1,23 +1,45 @@
-function plotSlipPlane (filename)
-%% plotDislocations (filename)
+function plotSlipPlane (filename, delimiter)
+%% plotDislocations (filename, delimiter)
 %  This function simply plots the positions (on the y-axis) of various
 %  defects present on a slip plane as a function of time (on the x-axis).
 %  The data is provided by the file whose name is given in the variable
 %  filename.
 %  The data in the file is presented in a row for each instant in time. The
 %  first column gives us the time and the remaining columns give us the
-%  positions.
+%  positions. The columns are separated by the character given by the
+%  argument delimiter.
 %  Since the function simply plots as crosses the entities present at a
 %  given time and does not try to connect the crosses across time, the
 %  order in which the entities appear is irrelevant.
 
+    %% Initialize the figure
+    figure;
+    hold on;
+
     %% Open the file
     fid = fopen (filename(:));
     
-    %% Read and parse line
+    %% Read the file one line at a time
     oneline = fgetl(fid);
     while ischar(oneline)
-        %
+        %% Parse line data into numbers
+        data = parseLineData(oneline, delimiter);
+        
+        %% Separate data
+        timeInstant = data(1,1);        % The first data is the time
+        nObjects = size(data,2) - 1;    % All the others are object positions
+        positions = data(1,2:end);
+        for i=1:nObjects
+            plot (timeInstant, positions(1,i), 'bx');
+        end
     end
+    
+    %% Close the file
+    fclose (fid);
+    
+    %% Release the figure
+    hold off;
+    xlabel('Time (s)');
+    ylabel('Positions (m)');
     
 end
